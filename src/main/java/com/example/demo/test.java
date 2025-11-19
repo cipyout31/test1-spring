c0ad77b5ea0d6412d25c008ffeb3bd3b66fa4da6
Devarakonda Harsha Vardan <109481657+cipyout31@users.noreply.github.com>
2025-11-19 21:46:12 +0530

    Update test.java

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class test {
     @GetMapping("/")
     String harsha(){
-        return "jaffa";
+        return "finally-working-fine";
     }
 }

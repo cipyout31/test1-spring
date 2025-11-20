4e1d1241f15d1b754fedf3fd7bc2905de6c948d7
Devarakonda Harsha Vardan <109481657+cipyout31@users.noreply.github.com>
2025-11-21 03:16:56 +0530

    Update test.java

--- src/main/java/com/example/demo/test.java
+++ src/main/java/com/example/demo/test.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class test {
     @GetMapping("/")
     String harsha(){
-        return "poiuyt";
+        return "harsha vardhan devarakonda";
     }
 }
